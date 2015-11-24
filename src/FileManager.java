3323163c01544c0a06de6088bc3f60cfef0e9c8f
Venkat Garapati <venkat.garapati.15@gmail.com>
2015-11-23 22:33:36 -0500

    Added Serialization

--- src/FileManager.java
+++ src/FileManager.java
@@ -10,8 +10,12 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.*;
 
 @SuppressWarnings("unused")
-public class FileManager {
+public class FileManager implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2292930000016794120L;
 	private String fileName;
 	private String filePath;
 	private String shortcut;
