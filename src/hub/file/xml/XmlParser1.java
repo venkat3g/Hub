c349a6a18ca7e2d091cd9240d7c537b11eabbecc
Venkat Garapati <venkat_garapati@hotmail.com>
2017-02-14 22:27:13 -0500

    Slight tweak to constructor

--- src/hub/file/xml/XmlParser1.java
+++ src/hub/file/xml/XmlParser1.java
@@ -16,72 +16,89 @@ import org.w3c.dom.Node;
 
 public class XmlParser1 {
 
-  private final File xmlFile;
-  private Node rootNode = null;
-  private final Document doc;
+    private final File xmlFile;
+    private Node rootNode = null;
+    private final Document doc;
 
-  /**
-   * XML Parsing class. Takes a file and returns the root node of the xml file.
-   * 
-   * @param file
-   *          file to check for root node.
-   * @throws Exception
-   *           exception if not able to find file.
-   */
-  public XmlParser1(File file) throws Exception {
-    xmlFile = file;
-
-    doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlFile);
-
-    rootNode = doc.getChildNodes().item(0);
-
-  }
-
-  public XmlParser1(File file, boolean bo) throws Exception {
-    xmlFile = file;
-
-    doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-
-    doc.appendChild(doc.createElement("Properties"));
-    
-    saveFile();
-
-    rootNode = doc.getChildNodes().item(0);
-  }
-
-  public Node getRootNode() {
-    return rootNode;
-  }
-
-  /**
-   * Saves the xml file.
-   * 
-   * @throws TransformerException
-   *           throws exception for being unable to save.
-   */
-  public void saveFile() throws TransformerException {
-    TransformerFactory transformerFactory = TransformerFactory.newInstance();
-    Transformer transformer = transformerFactory.newTransformer();
-    DOMSource source = new DOMSource(rootNode);
-
-    StreamResult streamResult = new StreamResult(xmlFile);
-
-    /*
-     * Adds whitespace to the output file. Does not properly add whitespace to
-     * first element.
+    /**
+     * XML Parsing class. Takes a file and returns the root node of the xml
+     * file.
+     * 
+     * @param file
+     *            file to check for root node.
+     * @throws Exception
+     *             exception if not able to find file.
      */
-    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-    transformer.transform(source, streamResult);
-  }
+    public XmlParser1(File file) throws Exception {
+        xmlFile = file;
 
-  public void removeNode(XmlNode1 xmlNode1) {
-    getRootNode().removeChild(xmlNode1.getNode());
-    
-  }
+        doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlFile);
 
-  public Element createElement(String tag) {
-    
-    return doc.createElement(tag);
-  }
+        rootNode = doc.getChildNodes().item(0);
+
+    }
+
+    /**
+     * XML Parsing class. Takes a file and returns the root node of the xml
+     * file.
+     * 
+     * @param file
+     *            file to check for root node.
+     * @param bo
+     *            boolean to notify if file exists or not.
+     * @throws Exception
+     *             exception if not able to find file.
+     */
+    public XmlParser1(File file, boolean bo) throws Exception {
+        xmlFile = file;
+
+        if (!bo) {
+            doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+        } else {
+            doc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+                    .parse(xmlFile);
+        }
+
+        doc.appendChild(doc.createElement("Properties"));
+
+        saveFile();
+
+        rootNode = doc.getChildNodes().item(0);
+    }
+
+    public Node getRootNode() {
+        return rootNode;
+    }
+
+    /**
+     * Saves the xml file.
+     * 
+     * @throws TransformerException
+     *             throws exception for being unable to save.
+     */
+    public void saveFile() throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(rootNode);
+
+        StreamResult streamResult = new StreamResult(xmlFile);
+
+        /*
+         * Adds whitespace to the output file. Does not properly add whitespace
+         * to first element.
+         */
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+        transformer.transform(source, streamResult);
+    }
+
+    public void removeNode(XmlNode1 xmlNode1) {
+        getRootNode().removeChild(xmlNode1.getNode());
+
+    }
+
+    public Element createElement(String tag) {
+
+        return doc.createElement(tag);
+    }
 }
